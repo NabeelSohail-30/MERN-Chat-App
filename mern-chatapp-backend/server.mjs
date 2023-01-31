2605a4c085f4460606591236a25d6af765f269ae
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-01-31 17:32:52 +0500

    changes in server

--- mern-chatapp-backend/server.mjs
+++ mern-chatapp-backend/server.mjs
@@ -2,38 +2,52 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import { Server } from 'socket.io'; //replaces (import socketIo from 'socket.io')
-import http from 'http';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 
-const rooms = ['general'];
+/* import authApis from './apis/auth.mjs';
+import tweetApi from './apis/tweet.mjs';
+import { userModel, messageModel } from "./dbRepo/models.mjs";
+import { stringToHash, varifyHash } from 'bcrypt-inzi'; */
 
-/*---------------------Server Creation----------------------------*/
+import { Server as socketIo } from 'socket.io';
+import { createServer } from "http";
+import cookie from 'cookie';
+
+const SECRET = process.env.SECRET || "topsecret";
+
 
 const app = express()
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(cors());
-
 const port = process.env.PORT || 5001;
 
-/* const io = socketIo(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
-    }
-}); */
 
-/*-----------------------------------------------------------------*/
+app.use(express.json());
+app.use(cookieParser());
 
-/*--------------------------------------------------------------*/
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://localhost:3000', "*"],
+    credentials: true
+}));
+
 
 const __dirname = path.resolve();
-app.get('/', express.static(path.join(__dirname, "/Web/index.html")));
-app.use('/', express.static(path.join(__dirname, "/Web")));
+app.use('/', express.static(path.join(__dirname, './web/build')))
+app.use('*', express.static(path.join(__dirname, './web/build')))
 
-app.listen(port, () => {
+
+
+
+// THIS IS THE ACTUAL SERVER WHICH IS RUNNING
+const server = createServer(app);
+
+// handing over server access to socket.io
+const io = new socketIo(server, {
+    cors: {
+        origin: ["http://localhost:3000", 'https://mern-chat-app-inzamam.up.railway.app'],
+        credentials: true
+    }
+});
+
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-/*-----------------------------------------------------------------*/
