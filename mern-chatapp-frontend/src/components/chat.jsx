e3a4506a20c96b922c2e7f0a7a6bebf153db170d
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-02-03 15:30:18 +0500

    experimenting to make a chat screen

--- mern-chatapp-frontend/src/components/chat.jsx
+++ mern-chatapp-frontend/src/components/chat.jsx
@@ -1,16 +1,66 @@
 import { Button, TextField, Box, IconButton } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import { Send } from "@mui/icons-material";
-//import "./chat.css";
 import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 
+const useStyles = makeStyles({
+  screen: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "50%",
+    margin: "auto",
+  },
+  messageWindow: {
+    width: "50rem",
+    height: "400px",
+    overflow: "scroll",
+    marginTop: "10px",
+    padding: "10px",
+    border: "1px solid #ddd",
+    borderRadius: "5px",
+    display: "flex",
+    flexDirection: "column",
+  },
+  leftBallon: {
+    backgroundColor: "#ddd",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "10px",
+    maxWidth: "60%",
+    alignSelf: "flex-start",
+  },
+  rightBallon: {
+    backgroundColor: "#03a9f4",
+    color: "white",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "10px",
+    maxWidth: "60%",
+    alignSelf: "flex-end",
+  },
+  textBox: {
+    display: "flex",
+    marginTop: "10px",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  field: {
+    width: "100%",
+  },
+});
+
 function Chat() {
+  const classes = useStyles();
   const [myMessages, setMyMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
 
   const scrollToBottom = () => {
     let messageWindow = document.querySelector("#messageWindow");
-    messageWindow.scrollTo(0, messageWindow.scrollHeight);
+    if (messageWindow) {
+      messageWindow.scrollTo(0, messageWindow.scrollHeight);
+    }
   };
 
   useEffect(() => {
@@ -45,12 +95,12 @@ function Chat() {
 
   return (
     <Box>
-      <div className="screen">
-        <div id="messageWindow" className="messageWindow">
+      <div className={classes.screen}>
+        <div id="messageWindow" className={classes.messageWindow}>
           {myMessages.map((eachMessage, key) => {
             if (eachMessage.from === "bot") {
               return (
-                <div key={key} className="leftBallon messageBallon">
+                <div key={key} className={classes.leftBallon}>
                   <div>
                     <b>{eachMessage.from}:</b> <span>{eachMessage.text}</span>
                   </div>
@@ -58,7 +108,7 @@ function Chat() {
               );
             } else {
               return (
-                <div key={key} className="rightBallon messageBallon">
+                <div key={key} className={classes.rightBallon}>
                   <div>
                     <b>{eachMessage.from}:</b> <span>{eachMessage.text}</span>
                   </div>
@@ -69,9 +119,9 @@ function Chat() {
         </div>
 
         <form onSubmit={sendMessage}>
-          <Box className="textBox " sx={{}}>
+          <Box className={classes.textBox} sx={{}}>
             <TextField
-              className="field"
+              className={classes.field}
               onChange={(e) => {
                 setNewMessage(e.target.value);
               }}
