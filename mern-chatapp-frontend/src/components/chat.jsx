dcd5408a7d6d5aede44eee40f9e269dd10651728
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-02-03 14:52:07 +0500

    more changes

--- mern-chatapp-frontend/src/components/chat.jsx
+++ mern-chatapp-frontend/src/components/chat.jsx
@@ -1,132 +1,93 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  Paper,
-  List,
-  ListItem,
-  ListItemAvatar,
-  Avatar,
-  ListItemText,
-  TextField,
-  Button,
-} from "@material-ui/core";
+import { Button, TextField, Box, IconButton } from "@mui/material";
+import { Send } from "@mui/icons-material";
+//import "./chat.css";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-  },
-  messageList: {
-    width: "100%",
-    height: "85%",
-    overflowY: "auto",
-    padding: "10px",
-  },
-  messageItem: {
-    display: "flex",
-    alignItems: "flex-start",
-    margin: "10px 0",
-  },
-  userMessage: {
-    backgroundColor: theme.palette.primary.main,
-    color: "#fff",
-    borderRadius: "20px 20px 0 20px",
-    padding: "10px 20px",
-    maxWidth: "70%",
-  },
-  botMessage: {
-    backgroundColor: theme.palette.secondary.light,
-    color: "#333",
-    borderRadius: "20px 20px 20px 0",
-    padding: "10px 20px",
-    maxWidth: "70%",
-  },
-  messageBox: {
-    width: "100%",
-    padding: "10px",
-    display: "flex",
-    alignItems: "center",
-    marginTop: "auto",
-  },
-  textField: {
-    flex: 1,
-    marginRight: "10px",
-  },
-  sendButton: {
-    backgroundColor: theme.palette.primary.main,
-    color: "#fff",
-    "&:hover": {
-      backgroundColor: theme.palette.primary.dark,
-    },
-  },
-}));
+function Chat() {
+  const [myMessages, setMyMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
 
-const Chat = () => {
-  const classes = useStyles();
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([
-    {
-      type: "bot",
-      text: "Hello, How can I help you today?",
-    },
-  ]);
+  const scrollToBottom = () => {
+    let messageWindow = document.querySelector("#messageWindow");
+    messageWindow.scrollTo(0, messageWindow.scrollHeight);
+  };
 
-  const handleSendMessage = () => {
-    if (message) {
-      setMessages([...messages, { type: "user", text: message }]);
-      setMessage("");
-    }
+  useEffect(() => {
+    scrollToBottom();
+  }, [myMessages]);
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    console.log("newMessage: ", newMessage);
+
+    setMyMessages((prev) => {
+      return [...prev, { text: newMessage, from: "user" }];
+    });
+
+    let response = await axios.post("http://localhost:5001/message", {
+      query: newMessage,
+    });
+
+    setMyMessages((prev) => {
+      return [
+        ...prev,
+        {
+          text: response?.data?.message?.text,
+          from: "bot",
+        },
+      ];
+    });
+
+    e.target.reset();
   };
 
   return (
-    <div className={classes.root}>
-      <Paper elevation={3} className={classes.messageList}>
-        <List>
-          {messages.map((message, index) => (
-            <ListItem key={index} className={classes.messageItem}>
-              {message.type === "user" && (
-                <ListItemAvatar>
-                  <Avatar>U</Avatar>
-                </ListItemAvatar>
-              )}
-              <ListItemText
-                primary={message.text}
-                className={
-                  message.type === "user"
-                    ? classes.userMessage
-                    : classes.botMessage
-                }
-              />
-              {message.type === "bot" && (
-                <ListItemAvatar>
-                  <Avatar>B</Avatar>
-                </ListItemAvatar>
-              )}
-            </ListItem>
-          ))}
-        </List>
-      </Paper>
-      <Paper elevation={3} className={classes.messageBox}>
-        <TextField
-          label="Enter message"
-          variant="outlined"
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          className={classes.textField}
-        />
-        <Button
-          variant="contained"
-          onClick={handleSendMessage}
-          className={classes.sendButton}
-        >
-          Send
-        </Button>
-      </Paper>
-    </div>
-  );
-};
+    <Box>
+      <div className="screen">
+        <div id="messageWindow" className="messageWindow">
+          {myMessages.map((eachMessage, key) => {
+            if (eachMessage.from === "bot") {
+              return (
+                <div key={key} className="leftBallon messageBallon">
+                  <div>
+                    <b>{eachMessage.from}:</b> <span>{eachMessage.text}</span>
+                  </div>
+                </div>
+              );
+            } else {
+              return (
+                <div key={key} className="rightBallon messageBallon">
+                  <div>
+                    <b>{eachMessage.from}:</b> <span>{eachMessage.text}</span>
+                  </div>
+                </div>
+              );
+            }
+          })}
+        </div>
 
+        <form onSubmit={sendMessage}>
+          <Box className="textBox " sx={{}}>
+            <TextField
+              className="field"
+              onChange={(e) => {
+                setNewMessage(e.target.value);
+              }}
+              id="outlined-basic"
+              placeholder="type a new message"
+              variant="outlined"
+            />
+            <Box>
+              <IconButton aria-label="send" size="large" type="submit">
+                <Send fontSize="inherit" />
+              </IconButton>
+            </Box>
+          </Box>
+        </form>
+      </div>
+    </Box>
+  );
+}
 export default Chat;
